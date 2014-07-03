922f5e3731096b3c1954bfb7649d93cdf97c39eb
Mike Nimer <mikenimer@yahoo.com>
2014-07-03 16:07:07 -0500

    Rework the code so the ExtractPdfImages unit test works.

--- src/main/java/apiserver/core/connectors/coldfusion/services/CollectionResult.java
+++ src/main/java/apiserver/core/connectors/coldfusion/services/CollectionResult.java
@@ -1,11 +1,13 @@
 package apiserver.core.connectors.coldfusion.services;
 
+import java.util.Collection;
+
 /**
  * Job that invokes a CFC that will return a Binary File
  * Created by mnimer on 4/17/14.
  */
-public interface BinaryJob
+public interface CollectionResult
 {
-    byte[] getPdfBytes();
-    void setPdfBytes(byte[] pdfBytes);
+    Collection getResult();
+    void setResult(Collection bytes);
 }
--- src/main/java/apiserver/core/connectors/coldfusion/services/ObjectResult.java
+++ src/main/java/apiserver/core/connectors/coldfusion/services/ObjectResult.java
@@ -4,7 +4,7 @@ package apiserver.core.connectors.coldfusion.services;
  * Job that invokes a CFC that will return data
  * Created by mnimer on 4/17/14.
  */
-public interface ObjectJob
+public interface ObjectResult
 {
     Object getResult();
     void setResult(Object object);
